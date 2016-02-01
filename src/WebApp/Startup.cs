91819a8f57b79c727b27e41cda78b1fe11afd98d
Lutando Ngqakaza <Lutz@whereismytransport.com>
2016-02-01 19:28:05 +0200

    first commit

---
+++ src/WebApp/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        
+
+        /*private IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment app)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(app.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            Configuration["wwwroot"] = env.WebRootPath;
+            Configuration["approot"] = app.ApplicationBasePath;
+        }*/
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();            
+            app.UseMvc();
+            app.UseStaticFiles();
+
+
+        }
+
+    }
+}
---
+++ src/WebApp/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp
+{
+    [Route("app")]
+    public class TestController : Controller
+    {
+        [HttpGet("thing", Name = "GetThing")]
+        public IActionResult Thing()
+        {
+            return new HttpOkResult();
+        }
+    }
+}
+
